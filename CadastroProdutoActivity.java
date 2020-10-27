cd19fe9cb7f6134b4f5db2d802d76ace92eb8a6a
douglasantunesd <70909360+douglasantunesd@users.noreply.github.com>
2020-10-26 21:07:28 -0300

    Add files via upload

---
+++ CadastroProdutoActivity.java
@@ -0,0 +1,61 @@
+package com.example.projeto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.projeto01.database.ProdutoDAO;
+import com.example.projeto01.modelo.Produto;
+
+public class CadastroProdutoActivity extends AppCompatActivity {
+
+    private int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_produto);
+        setTitle("Cadastro de Produto");
+        carregarProduto();
+    }
+
+    public  void carregarProduto(){
+        Intent intent = getIntent();
+        if ( intent != null && intent.getExtras() != null && intent.getExtras().get("produtoEdicao") != null){
+            Produto produto = (Produto) intent.getExtras().get("produtoEdicao");
+            EditText editTextNome = findViewById(R.id.editTextT_nome);
+            EditText editTextValor = findViewById(R.id.editText_valor);
+            editTextNome.setText(produto.getNome());
+            editTextValor.setText(String.valueOf(produto.getValor()));
+            id = produto.getId();
+
+        }
+    }
+
+
+    public void onClickVoltar(View v){
+        finish();
+    }
+
+    public void onClickSalvar(View v) {
+        EditText editTextNome = findViewById(R.id.editTextT_nome);
+        EditText editTextValor = findViewById(R.id.editText_valor);
+
+        String nome = editTextNome.getText().toString();
+        Float valor = Float.parseFloat(editTextValor.getText().toString());
+
+        Produto produto = new Produto(id,nome, valor);
+        ProdutoDAO produtoDAO = new ProdutoDAO(getBaseContext());
+        boolean salvou = produtoDAO.salvar(produto);
+        if (salvou){
+            finish();
+        } else {
+            Toast.makeText(CadastroProdutoActivity.this, "ERRO ao Salvar", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.projeto01;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.projeto01.database.ProdutoDAO;
+import com.example.projeto01.modelo.Produto;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ListView listViewProdutos;
+    private ArrayAdapter<Produto> adapterProdutos;
+    private int id = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Produtos");
+        listViewProdutos = findViewById(R.id.listView_produtos);
+        definirOnLongClickListener();
+        definirOnClickListenerListView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ProdutoDAO produtoDAO = new ProdutoDAO(getBaseContext());
+        adapterProdutos = new ArrayAdapter<Produto>(MainActivity.this, android.R.layout.simple_list_item_1, produtoDAO.listar());
+        listViewProdutos.setAdapter(adapterProdutos);
+    }
+
+    private void definirOnClickListenerListView() {
+        listViewProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produto produtoClicado = adapterProdutos.getItem(position);
+                Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+                intent.putExtra("produtoEdicao", produtoClicado);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void definirOnLongClickListener() {
+        listViewProdutos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Produto produtoClicadoLongo = adapterProdutos.getItem(position);
+                ProdutoDAO produtoDAO = new ProdutoDAO(getBaseContext());
+
+                if(produtoDAO.excluir(produtoClicadoLongo)) {
+                    Toast.makeText(getApplicationContext(), "Produto excluido!", Toast.LENGTH_LONG).show();
+                    onResume();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Erro ao excluir!", Toast.LENGTH_LONG).show();
+                }
+                return true;
+            }
+        });
+    }
+
+    public  void onClickNovoProduto(View v){
+        Intent intent = new Intent(MainActivity.this, CadastroProdutoActivity.class);
+        startActivity(intent);
+    }
+}
